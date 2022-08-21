3256191f92cda30484eada4dbc8ff78b3feeb063
Canaan <reakak1@gmail.com>
2022-08-21 21:57:34 +0300

    Added Dept tests

---
+++ ApplicationTests/Depts/EditTest.cs
@@ -0,0 +1,62 @@
+﻿using Application.Depts;
+using Application.DTOs;
+
+using Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTests.Depts
+{
+    public class EditTest : TestBase
+    {
+        [Fact]
+        public void Should_Be_Able_To_Edit()
+        {
+            var context = GetDbContext();
+            Guid id = new Guid("69f31c5d-5872-4d86-841b-9bca0a2bc79e");
+
+            context.Users.AddAsync(new Domain.AppUser { Id = "1", Email = "test@example.com", UserName = "test", DisplayName = "test" });
+            context.SaveChangesAsync();
+
+            context.Depts.Add(new Dept
+            {
+                Id = id,
+                Name = "Example A",
+                Amount = 4550,
+                AmountRemaining = 2880,
+                DateDeptWasMade = DateTime.UtcNow,
+                DatePaidOff = DateTime.UtcNow,
+                PaidOff = false,
+                Deptor = "User1",
+                UserId = "1"
+            });
+
+            context.SaveChanges();
+
+            var deptInfo = new DeptWriteDto 
+            {
+                Name = "Example A",
+                Amount = 4550,
+                AmountRemaining = 1550,
+                DateDeptWasMade = DateTime.UtcNow,
+                DatePaidOff = DateTime.UtcNow,
+                PaidOff = false,
+                Deptor = "User1",
+            };
+            
+            var sut = new Edit.Handler(context, _mapper, userAccessor.Object);
+
+            var result = sut.Handle(new Edit.Command { Id = id, Dept = deptInfo }, CancellationToken.None).Result;
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+
+            Assert.Equal("Example A", result.Value.Name);
+
+        }
+    }
+}
---
+++ ApplicationTests/Depts/ListTest.cs
@@ -0,0 +1,85 @@
+﻿using Application.Depts;
+
+using Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ApplicationTests.Depts
+{
+    public class ListTest : TestBase
+    {
+        [Fact]
+        public void Should_Return_List()
+        {
+            var context = GetDbContext();
+
+            context.Users.AddAsync(new Domain.AppUser { Id = "1", Email = "test@example.com", UserName = "test", DisplayName = "test" });
+            context.SaveChangesAsync();
+
+
+            context.Depts.Add(new Dept {
+                    Name = "Example A",
+                    Amount = 4550,
+                    AmountRemaining = 2880,
+                    DateDeptWasMade = DateTime.UtcNow,
+                    DatePaidOff = DateTime.UtcNow,
+                    PaidOff = false,
+                    Deptor = "User1",
+                    UserId = "1"
+                 });
+            context.Depts.Add(new Dept
+            {
+                Name = "Example B",
+                Amount = 90,
+                AmountRemaining = 0.0,
+                DateDeptWasMade = DateTime.UtcNow,
+                DatePaidOff = DateTime.UtcNow,
+                PaidOff = true,
+                Deptor = "User1",
+                UserId = "1"
+            });
+
+            context.Depts.Add(new Dept
+            {
+                Name = "Example B",
+                Amount = 666,
+                AmountRemaining = 0.0,
+                DateDeptWasMade = DateTime.UtcNow,
+                DatePaidOff = DateTime.UtcNow,
+                PaidOff = true,
+                Deptor = "Dark Schinder",
+                UserId = "2"
+            });
+
+            context.Depts.Add(new Dept
+            {
+                Name = "Example C",
+                Amount = 92103,
+                AmountRemaining = 91000,
+                DateDeptWasMade = DateTime.UtcNow,
+                DatePaidOff = DateTime.UtcNow,
+                PaidOff = false,
+                Deptor = "redGrave Pizzaria",
+                UserId = "3"
+            });
+
+            context.SaveChanges();
+
+
+            var sut = new List.Handler(context, _mapper, userAccessor.Object);
+            var result = sut.Handle(new List.Query { Params=new Application.Core.PagedParams { } }, CancellationToken.None).Result;
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Value.Count);
+            Assert.Equal("Example A", result.Value[0].Name);
+        }
+
+        //TODO: mopre testing 
+    }
+}
